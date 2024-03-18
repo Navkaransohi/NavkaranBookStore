454a49d8f966f2c96a0ce00eea111174d1d2b24e
Navkaransohi <sohinavkaran@gmail.com>
2024-03-17 21:24:51 -0400

    Add project files.

---
+++ NavkaranBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NavkaranBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
